8ac2c3bebbefd26a32a7b31d2958c166980b5040
Paulo Grahl <pgrahl@gmail.com>
2020-02-11 13:40:48 -0300

    Initial commit from local project

---
+++ run_all.R
@@ -0,0 +1,15 @@
+
+####################################
+# Breakeven Claims                 #
+# Need to update: both excel files # 
+####################################
+rmarkdown::render(
+  "rmds/us_breakeven_claims_initial.Rmd",
+  encoding = "UTF-8",
+  output_format = NULL,
+  output_dir = "reports",
+  output_file = "Breakeven_Claims_Report_Oct2019.pdf",
+  intermediates_dir = "temp_files",
+  clean = FALSE,
+  envir = new.env()
+)
